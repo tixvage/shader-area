ab98518f8facd1c665198f2fc09ba423c06508be
melih <melihbykr00@gmail.com>
2025-07-30 12:38:51 +0300

    add sun shader

---
+++ shaders/sun.fs
@@ -0,0 +1,22 @@
+#version 330
+
+in vec2 fragTexCoord;
+in vec4 fragColor;
+
+uniform sampler2D texture0;
+
+out vec4 finalColor;
+
+void main()
+{
+    vec4 texelColor = texture(texture0, fragTexCoord);
+    vec2 uv = fragTexCoord - vec2(0.5);
+
+    float d = length(uv);
+    d = 0.3 / d;
+    d -= 0.1;
+    vec3 color = vec3(d, d * 0.8, d * 0.3);
+    color *= 0.7;
+
+    finalColor = vec4(color, 1);
+}
